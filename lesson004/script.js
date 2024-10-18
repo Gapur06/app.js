db6be77a1eb52c9a396baf770dba768f96faf226
Gapur06 <gapur009956@gmail.com>
2024-10-18 16:52:16 +0300

    add lesson004

---
+++ lesson004/script.js
@@ -0,0 +1,29 @@
+const car = {
+
+  brand: "Mercedes Benz",
+  model: "s600",
+  year: 2024
+};
+console.log(car)
+
+
+const person = {
+
+name: 'Igor',
+age: 29,
+
+};
+person.city = 'Moscow';
+console.log(person)
+
+let fruits = ["яблоко", "банан", "апельсин"];
+console.log(fruits[0])
+console.log(fruits[2])
+
+const colors = ["красный", "синий", "зеленый"]
+colors.push("желтый")
+console.log(colors)
+
+let animals = ["кот", "собака", "птица"]
+animals.pop()
+console.log(animals)
