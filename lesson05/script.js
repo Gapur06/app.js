560d192fe72c31eb8b0066b03681ce87c574f67d
Gapur06 <gapur009956@gmail.com>
2024-10-20 16:09:46 +0300

    lesson05

---
+++ lesson05/script.js
@@ -0,0 +1,18 @@
+let i = 1;
+while (i <= 10) {
+  console.log(i)
+  i++
+};
+
+
+
+for (let i = 2; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+
+for (i=10; i>0; i--) {
+  console.log(i);
+  }
