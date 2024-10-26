f76028e155ba038e9a495bd44c45612f20818559
Gapur06 <gapur009956@gmail.com>
2024-10-26 14:16:16 +0300

    add lesson06

---
+++ lesson06/script.js
@@ -0,0 +1,30 @@
+function sum(a, b) {
+
+console.log(a + b)
+
+}
+
+sum(5,6)
+
+
+
+let number  = prompt ('Напиши число')
+
+function isEven() { 
+  if (number % 2 === 0){
+    alert(true)
+  }
+  else {
+    alert(false)
+  }
+  }
+  isEven()
+
+
+
+let a = +prompt("Введите первое число", "");
+let b = +prompt("Введите второе число", "");
+
+alert( a + b );
+
+ 
